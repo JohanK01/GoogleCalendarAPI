82b38ac0f04a263c121fb6e212705e6a3f3e510c
JohanK <klausenjohan23@gmail.com>
2021-10-18 10:02:10 +0200

    ready to go

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8082;
+
+const appRouter = require('./router/app');
+
+app.use(express.urlencoded());
+
+app.use(express.static('public'));
+
+app.use('/', appRouter);
+
+app.listen(PORT, () => {
+    console.log(`the app listen to ${PORT}`);
+})
